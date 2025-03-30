0878c3ca4d34285739bc3f9f6b9fb7e6350472ff
vatsalllll <vatsal20202@gmail.com>
2025-03-30 17:27:50 +0530

    version 0 completed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 import { LinkField } from "@prismicio/client";
 import { PrismicNextLink } from "@prismicio/next";
 import clsx from "clsx";
+import Link from "next/link";
 
 type Props = {
   buttonLink: LinkField;
@@ -10,14 +11,16 @@ type Props = {
 
 export default function Button({ buttonLink, buttonText, className }: Props) {
   return (
-    <PrismicNextLink
+    <Link
       className={clsx(
         "rounded-xl bg-orange-600 px-5 py-4 text-center text-xl font-bold uppercase tracking-wide text-white transition-colors duration-150 hover:bg-orange-700 md:text-2xl",
         className,
       )}
-      field={buttonLink}
+      href="https://github.com/vatsalllll"
+      target="_blank"
+      rel="noopener noreferrer"
     >
       {buttonText}
-    </PrismicNextLink>
+    </Link>
   );
 }
